59ca324633ed77c8db95de7cf8ebb12c29f3929b
Andy Stanciu <andreist@outlook.com>
2024-11-11 14:59:33 -0800

    Merge solutions

--- src/scraper/scrape.sh
+++ src/scraper/scrape.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 # number of problems
-COUNT=1500
+COUNT=900
 
 python3 scraper.py \
-  -p climbing-stairs -c $COUNT \
-#  -p two-sum -c $COUNT \
-#  -p add-two-numbers -c $COUNT \
-#  -p median-of-two-sorted-arrays -c $COUNT \
-#  -p binary-tree-preorder-traversal -c $COUNT \
-#  -p binary-tree-postorder-traversal -c $COUNT \
-#  -p binary-tree-inorder-traversal -c $COUNT \
+  -p majority-element-ii -c $COUNT
