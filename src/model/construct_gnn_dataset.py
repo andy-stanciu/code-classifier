cda9afeae94c999a8b92c4dba99bbb0e3e26d02a
Andy Stanciu <andreist@outlook.com>
2024-11-30 15:06:53 -0800

    More training progress

--- src/model/construct_gnn_dataset.py
+++ src/model/construct_gnn_dataset.py
@@ -10,55 +10,7 @@ import matplotlib.pyplot as plt
 import pickle
 
 TOTAL_SOLUTION_COUNT = 500
-PROBLEM_COUNT = 10
-
-FILES = [
-'koko-eating-bananas',
-'two-sum', 
-'binary-tree-preorder-traversal',
-'binary-tree-postorder-traversal',
-'merge-k-sorted-lists',
-
-'minimum-number-of-arrows-to-burst-balloons',
-'kth-smallest-element-in-a-bst',
-'intersection-of-two-arrays',
-'longest-palindromic-substring',
-'combinations',
-
-'3sum',
-'4sum', 
-'majority-element',
-'generate-parentheses',
-'valid-anagram',
-
-'product-of-array-except-self',
-'median-of-two-sorted-arrays',
-'integer-to-roman', 
-'single-number',
-'longest-consecutive-sequence', 
-
-'sqrtx', 'find-the-index-of-the-first-occurrence-in-a-string',  
- 'plus-one', 'linked-list-cycle', 'jump-game',   
-'minimum-size-subarray-sum',  'unique-paths-ii', 'permutations', 'coin-change', 
-'group-anagrams', 'maximum-subarray', 'counting-bits', 'remove-nth-node-from-end-of-list', 'longest-common-prefix', 
- 'remove-duplicates-from-sorted-array-ii', 'is-subsequence', 
-'best-time-to-buy-and-sell-stock', 'odd-even-linked-list', 'game-of-life', 'happy-number', 'valid-palindrome', 
-'sum-of-left-leaves', 'find-minimum-in-rotated-sorted-array', 'first-missing-positive',  
-'binary-tree-zigzag-level-order-traversal', 'average-of-levels-in-binary-tree', 'remove-duplicates-from-sorted-array', 
-'combination-sum-ii', 'target-sum',  'merge-sorted-array', 
-'evaluate-reverse-polish-notation', 'reverse-nodes-in-k-group',  '3sum-closest', 'swap-nodes-in-pairs', 
-'word-pattern', 'palindrome-partitioning', 'reverse-linked-list',  'count-complete-tree-nodes', 
-'best-time-to-buy-and-sell-stock-ii', 'pascals-triangle', 'delete-node-in-a-bst', 'move-zeroes', 'missing-number', 
-'course-schedule',  'third-maximum-number', 'sort-characters-by-frequency', 
-'climbing-stairs', 'linked-list-cycle-ii', 'permutations-ii', 'course-schedule-ii', 
-'kth-largest-element-in-an-array', 'symmetric-tree', 'find-pivot-index', 'non-overlapping-intervals', 'rotate-image', 
-'path-sum-ii', 'gas-station',  'zigzag-conversion', 'asteroid-collision', 'majority-element-ii', 
-'powx-n', 'longest-increasing-subsequence', 'reverse-words-in-a-string', 'minimum-window-substring', 'next-permutation', 
-'sort-colors', 'add-two-numbers-ii', 'word-break', 'string-compression', 'merge-two-sorted-lists', 'binary-tree-paths', 
-'search-in-rotated-sorted-array', 'palindrome-number', 'path-sum', 'binary-tree-inorder-traversal', 
-'binary-tree-maximum-path-sum', 'contains-duplicate-ii', 'ransom-note', 'add-two-numbers', 
-'intersection-of-two-arrays-ii', 
-]
+PROBLEM_COUNT = 100
 
 class SolutionDataset(Dataset):
     def __init__(self, root, transform=None, pre_transform=None):
@@ -116,30 +68,35 @@ class SolutionDataset(Dataset):
     
     def get_solution_path(self, idx):
         return self.solution_files[idx]
+    
 
-raw_solutions = SolutionDataset(root='../../data/raw')
-print(f'total solution count: {len(raw_solutions)}')
+def main():
+    raw_solutions = SolutionDataset(root='../../data/raw')
+    print(f'total solution count: {len(raw_solutions)}')
 
-with open('../../data/model-pickle/solutions_dataset_gnn_graphs.pkl', 'wb') as f:
-    pickle.dump(raw_solutions, f)
+    with open('../../data/model-pickle/solutions_dataset_gnn_graphs.pkl', 'wb') as f:
+        pickle.dump(raw_solutions, f)
 
-print("Dataset serialized and saved to '../../data/model-pickle/solutions_dataset_gnn_graphs.pkl'")
+    print("Dataset serialized and saved to '../../data/model-pickle/solutions_dataset_gnn_graphs.pkl'")
 
-# sample 1 random solution
-solution_nums = random.sample(range(0, len(raw_solutions)), k=1)
-for solution_num in solution_nums:
-    raw_solutions.visualize(solution_num)
-    data = raw_solutions.get(solution_num)
-    solution, _, _ = raw_solutions.get_solution_path(solution_num)
-    print('==========================')
-    print(f'Solution: {solution}')
-    print(data)
-    print(f'Number of nodes: {data.num_nodes}')
-    print(f'Number of edges: {data.num_edges}')
-    print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}')
-    print(f'Has isolated nodes: {data.has_isolated_nodes()}')
-    print(f'Has self-loops: {data.has_self_loops()}')
-    print(f'Is undirected: {data.is_undirected()}')
-    print(f'x: {data.x}')
-    print(f'y: {data.y}')
-    print('==========================')
+    # sample 1 random solution
+    solution_nums = random.sample(range(0, len(raw_solutions)), k=1)
+    for solution_num in solution_nums:
+        raw_solutions.visualize(solution_num)
+        data = raw_solutions.get(solution_num)
+        solution, _, _ = raw_solutions.get_solution_path(solution_num)
+        print('==========================')
+        print(f'Solution: {solution}')
+        print(data)
+        print(f'Number of nodes: {data.num_nodes}')
+        print(f'Number of edges: {data.num_edges}')
+        print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}')
+        print(f'Has isolated nodes: {data.has_isolated_nodes()}')
+        print(f'Has self-loops: {data.has_self_loops()}')
+        print(f'Is undirected: {data.is_undirected()}')
+        print(f'x: {data.x}')
+        print(f'y: {data.y}')
+        print('==========================')
+
+if __name__ == "__main__":
+    main()
